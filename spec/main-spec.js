d66e6a19bc473c8b7070121e6184f9e3b0449235
HaoWang-TW <hawang@thoughtworks.com>
2018-05-19 23:55:42 +0800

    add test for distance less than 2km and no waitting time

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,9 @@
 const main = require('../main/main');
 
 describe('taxi fee', function () {
-    // write your tests here...
+    it('should return 6 if distance is less than 2km, and taxi driver didn\'t wait', () => {
+        expect(main(0, 0)).toBe(6);
+        expect(main(1, 0)).toBe(6);
+        expect(main(1.5, 0)).toBe(6);
+    })
 });
