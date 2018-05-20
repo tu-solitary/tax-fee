10236398f8d7dc81129d39989436c17064dbd33c
HaoWang-TW <hawang@thoughtworks.com>
2018-05-20 09:19:50 +0800

    add test for 2 < distance <= 8

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -5,5 +5,11 @@ describe('taxi fee', function () {
         expect(main(0, 0)).toBe(6);
         expect(main(1, 0)).toBe(6);
         expect(main(1.5, 0)).toBe(6);
+    });
+
+    it('should charge for 0.8/km if distance is more than 2km and less than 8km', () => {
+        expect(main(2, 0)).toBe(6);
+        expect(main(3, 0)).toBe(6.8);
+        expect(main(8, 0)).toBe(10.8);
     })
 });
