114473deb72a1700a36e7dd4c30340ce47419f2d
HaoWang-TW <hawang@thoughtworks.com>
2018-05-20 10:53:05 +0800

    modify test for rounded totalFee

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -9,18 +9,18 @@ describe('taxi fee', function () {
 
     it('should charge for 0.8/km if distance is more than 2km and less than 8km', () => {
         expect(main(2, 0)).toBe(6);
-        expect(main(3, 0)).toBe(6.8);
-        expect(main(8, 0)).toBe(10.8);
+        expect(main(3, 0)).toBe(7);
+        expect(main(8, 0)).toBe(11);
     });
 
     it('should charge for 50% more if distance is further than 8km', () => {
-        expect(main(9, 0)).toBeCloseTo(12, 4);
-        expect(main(10, 0)).toBeCloseTo(13.2, 4); // 6 + 8 * 0.8 + 2 * 0.4
+        expect(main(9, 0)).toBe(12);
+        expect(main(10, 0)).toBe(13); // 6 + 8 * 0.8 + 2 * 0.4
     });
     it('should charge 0.25 per minute if taxi have waittng time', () => {
-        expect(main(0, 10)).toBeCloseTo(8.5, 4);
-        expect(main(2, 10)).toBeCloseTo(8.5, 4);
-        expect(main(10, 10)).toBeCloseTo(15.7, 4);
+        expect(main(0, 10)).toBe(9);
+        expect(main(2, 10)).toBe(9);
+        expect(main(10, 10)).toBe(16);
 
     })
 });
