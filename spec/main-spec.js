1fdaa127e1a2f1acc970578e4b3855ba1d7c7c94
HaoWang-TW <hawang@thoughtworks.com>
2018-05-20 10:46:00 +0800

    add test for distance further than 8km

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -11,5 +11,10 @@ describe('taxi fee', function () {
         expect(main(2, 0)).toBe(6);
         expect(main(3, 0)).toBe(6.8);
         expect(main(8, 0)).toBe(10.8);
+    });
+
+    it('should charge for 50% more if distance is further than 8km', () => {
+        expect(main(9, 0)).toBeCloseTo(12, 4);
+        expect(main(10, 0)).toBeCloseTo(13.2, 4); // 6 + 8 * 0.8 + 2 * 0.4
     })
 });
