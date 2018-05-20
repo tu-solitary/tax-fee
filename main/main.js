89446f25c259b6bc9a43a7c825ddfbe8597a1f56
HaoWang-TW <hawang@thoughtworks.com>
2018-05-20 09:20:31 +0800

    add some code to implement situation when 2 < distance < 8

--- main/main.js
+++ main/main.js
@@ -1,5 +1,9 @@
 module.exports = function main(distance, waitingTime) {
+    let totalFee = 0;
     if(distance <= 2 && waitingTime === 0) {
         return 6;
+    } else if(distance <= 8) {
+        return 6 + (distance - 2) * 0.8;
     }
+
 };
